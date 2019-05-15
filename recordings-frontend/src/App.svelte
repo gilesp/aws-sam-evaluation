a8cbe48878a303f07bcf28e65b2da74fba73b066
Giles Paterson <giles.paterson@blackpepper.co.uk>
2019-05-15 09:52:12 +0100

    Initial svelte frontend for accessing recordings & transcriptions

---
+++ recordings-frontend/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+ import { onMount } from "svelte";
+ import Recording from "./Recording.svelte";
+ 
+ let recordings = [];
+
+ onMount(async () => {
+   const response = await fetch(`http://localhost:3000/listRecordings`);
+   recordings = await response.json();
+ });
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+
+ Recording {
+   margin-bottom: 5px;
+ }
+</style>
+
+<h1>Recordings</h1>
+
+<div class="recordings">
+  {#each recordings as recording}
+  <Recording name={recording.name} date={recording.modified} url={recording.url} />
+  {:else}
+  <p>loading...</p>
+  {/each}
+</div>
---
+++ recordings-frontend/src/Recording.svelte
@@ -0,0 +1,38 @@
+<script>
+ export let name;
+ export let date;
+ export let url;
+
+ const recordingDate = new Date(date);
+</script>
+
+<style>
+ .recording {
+   width: 300px;
+   border: 1px solid #aaa;
+   border-radius: 2px;
+   box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+   padding: 1em;
+ }
+
+ h2 {
+   padding: 0 0 0.2em 0;
+   margin: 0 0 1em 0;
+   border-bottom: 1px solid #ff3e00
+ }
+
+ audio, .date {
+   margin: 0 0 0.5em 0;
+   line-height: 1.2;
+ }
+</style>
+
+<article class="recording" >
+  <h2>{name.substring(11)}</h2>
+  <audio controls src={url}>
+    Your browser does not support the <code>audio</code> element.
+  </audio>
+  <div class="date">
+    {recordingDate.toLocaleString()}
+  </div>
+</article>
